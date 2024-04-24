b28a723066f9e59fa9c3f7bc58548d3f38c1db4d
KK <kf2@karfield.com>
2024-04-24 20:59:02 +0800

    add try_parse_eq()
    Signed-off-by: KK <kf2@karfield.com>

--- src/try_parse_tokens.rs
+++ src/try_parse_tokens.rs
@@ -5,6 +5,7 @@ pub trait TryParseTokens {
     fn try_parse_colon(&self) -> Option<Token![:]>;
     fn try_parse_semi(&self) -> Option<Token![;]>;
     fn try_parse_dot(&self) -> Option<Token![.]>;
+    fn try_parse_eq(&self) -> Option<Token![=]>;
 }
 
 impl TryParseTokens for ParseBuffer<'_> {
@@ -35,6 +36,14 @@ impl TryParseTokens for ParseBuffer<'_> {
     fn try_parse_dot(&self) -> Option<Token![.]> {
         if self.peek(Token![.]) {
             self.parse::<Token![.]>().ok()
+        } else {
+            None
+        }
+    }
+
+    fn try_parse_eq(&self) -> Option<Token![=]> {
+        if self.peek(Token![=]) {
+            self.parse::<Token![=]>().ok()
         } else {
             None
         }
