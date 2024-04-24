5f5268e39f8d826aa784ec82d80e171bac698eb5
KK <kf2@karfield.com>
2024-04-24 19:48:49 +0800

    ready for release 0.1.5
    Signed-off-by: KK <kf2@karfield.com>

---
+++ src/try_parse_tokens.rs
@@ -0,0 +1,42 @@
+use syn::{parse::ParseBuffer, Token};
+
+pub trait TryParseTokens {
+    fn try_parse_comma(&self) -> Option<Token![,]>;
+    fn try_parse_colon(&self) -> Option<Token![:]>;
+    fn try_parse_semi(&self) -> Option<Token![;]>;
+    fn try_parse_dot(&self) -> Option<Token![.]>;
+}
+
+impl TryParseTokens for ParseBuffer<'_> {
+    fn try_parse_comma(&self) -> Option<Token![,]> {
+        if self.peek(Token![,]) {
+            self.parse::<Token![,]>().ok()
+        } else {
+            None
+        }
+    }
+
+    fn try_parse_colon(&self) -> Option<Token![:]> {
+        if self.peek(Token![:]) {
+            self.parse::<Token![:]>().ok()
+        } else {
+            None
+        }
+    }
+
+    fn try_parse_semi(&self) -> Option<Token![;]> {
+        if self.peek(Token![;]) {
+            self.parse::<Token![;]>().ok()
+        } else {
+            None
+        }
+    }
+
+    fn try_parse_dot(&self) -> Option<Token![.]> {
+        if self.peek(Token![.]) {
+            self.parse::<Token![.]>().ok()
+        } else {
+            None
+        }
+    }
+}
