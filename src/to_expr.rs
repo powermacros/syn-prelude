e9857b0c24098e1325787fbc116aa97e4a5aee74
KK <kf2@karfield.com>
2024-04-30 16:20:02 +0800

    add to_expr for vec<ToExpr>
    Signed-off-by: KK <kf2@karfield.com>

--- src/to_expr.rs
+++ src/to_expr.rs
@@ -1,6 +1,8 @@
+use quote::quote_spanned;
 use syn::{
-    Expr, ExprLit, ExprPath, Lit, LitBool, LitByte, LitByteStr, LitChar, LitFloat, LitInt, LitStr,
-    Path,
+    punctuated::Punctuated, spanned::Spanned, token::Bracket, Expr, ExprLit, ExprMacro, ExprPath,
+    Ident, Lit, LitBool, LitByte, LitByteStr, LitChar, LitFloat, LitInt, LitStr, Macro, Path,
+    PathSegment, Token,
 };
 
 pub trait ToExpr {
@@ -149,6 +151,33 @@ impl ToExpr for &Path {
             attrs: vec![],
             qself: None,
             path: self.clone(),
+        })
+    }
+}
+
+impl<T: ToExpr> ToExpr for Vec<T> {
+    fn to_expr(self) -> Expr {
+        let exprs = self.into_iter().map(|x| x.to_expr()).collect::<Vec<_>>();
+        let span = exprs.first().map(|x| x.span()).unwrap();
+
+        Expr::Macro(ExprMacro {
+            attrs: vec![],
+            mac: Macro {
+                path: Path {
+                    leading_colon: None,
+                    segments: {
+                        let mut segments = Punctuated::new();
+                        segments.push(PathSegment {
+                            ident: Ident::new("vec", span),
+                            arguments: syn::PathArguments::None,
+                        });
+                        segments
+                    },
+                },
+                bang_token: Token![!](span),
+                delimiter: syn::MacroDelimiter::Bracket(Bracket(span)),
+                tokens: quote_spanned!(span => #(#exprs),*).into(),
+            },
         })
     }
 }
